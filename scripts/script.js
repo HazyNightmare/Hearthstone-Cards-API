dcd2107f781bfaaf96e785b8a7e8495e19fcf6f3
Zach Surgett <zsurgett@gmail.com>
2018-01-09 18:56:21 -0700

    Very basic search field that loads cards that match the query

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
 $(function() {
   'use strict';
-  var cardHolder = $(".cardHolder");
-  var linkText = "";
+  let cardHolder = $(".cardHolder");
+  let imgLocation = "HSCardImgs/";
+  let searchInput = $(".searchInput");
+  let button = $(".button");
 
-  $.getJSON("https://api.hearthstonejson.com/v1/18336/enUS/cards.json", function(json) {
-  });
+  $(button).click(function() {
+    $(cardHolder).children().remove(".card");
+    let search = $(searchInput).val();
+    if (search !== null && search !== undefined && search !== "") {
+      $.getJSON("https://api.hearthstonejson.com/v1/18336/enUS/cards.json", function(json) {
+        for (var i = 0; i < json.length; i++) {
+          let name = json[i].name;
+          if (name.substr(0, search.length) === search) {
+            $(cardHolder).append("<img class='card' src='" + imgLocation + json[i].id + ".png' >");
+          }
+        }
+      });
+    }
+    else {
+      $(cardHolder).append("<div><p>There was an error. Try again</p></div>");
+    }
+  })
 });
